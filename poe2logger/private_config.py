d896b10cd7850e8a13004599ce15c248f5b61cce
Hubert Dylewski <dylek.pl@gmail.com>
2026-06-16 01:57:09 +0200

    Added private config stub

---
+++ poe2logger/private_config.py
@@ -0,0 +1,11 @@
+from datetime import timezone, timedelta
+
+
+LEAGUE_NAME = 'Runes of Aldur'
+GUILD_ID = ''
+SESSION_ID = ''
+DISPLAY_TIMEZONE = timezone(timedelta(hours=2), 'CEST')
+TARGET_STASHES = {'$$$', 'Deli', 'Ess', 'Aug', 'Ritual', 'Abbys/Expedition'}
+STASH_ALIASES = {
+    'Abbys/Expediton': 'Abbys/Expedition',
+}
